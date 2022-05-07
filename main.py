ffdfd7b08b2f0788a3f6ce0e7c35aa3353e0fe92
Graham P Wynes <wynesg@uwplatt.edu>
2022-05-07 17:07:15 -0500

    solve easy puzzles

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import numpy as np
+from board import Board
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+puzzle = np.matrix([[0, 0, 3, 0, 2, 0, 6, 0, 0],
+                    [9, 0, 0, 3, 0, 5, 0, 0, 1],
+                    [0, 0, 1, 8, 0, 6, 4, 0, 0],
+                    [0, 0, 8, 1, 0, 2, 9, 0, 0],
+                    [7, 0, 0, 0, 0, 0, 0, 0, 8],
+                    [0, 0, 6, 7, 0, 8, 2, 0, 0],
+                    [0, 0, 2, 6, 0, 9, 5, 0, 0],
+                    [8, 0, 0, 2, 0, 3, 0, 0, 9],
+                    [0, 0, 5, 0, 1, 0, 3, 0, 0]])
 
+board = Board(puzzle)
+print(board.get_layout())
+zero_count = 0
+for y in range(9):
+    for x in range(9):
+        if board.get_layout()[y, x] == 0:
+            zero_count = zero_count + 1
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while zero_count != 0:
+    for y in range(9):
+        for x in range(9):
+            if board.get_layout()[y,x] == 0:
+                if len(board.get_domain(x,y)) == 1:
+                    print(x)
+                    print(y)
+                    answer = board.get_domain(x,y)
+                    print(answer)
+                    board.get_layout()[y,x] = answer[0]
+                    zero_count = zero_count - 1
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(board.get_layout())
